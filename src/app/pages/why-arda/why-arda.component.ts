f981f135e23bf9d33ae97a514dd479887ba87ea9
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-09-12 20:34:00 -0300

    chore: add why arda section to the project

---
+++ src/app/pages/why-arda/why-arda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-why-arda',
+  templateUrl: './why-arda.component.html',
+  styleUrls: ['./why-arda.component.scss']
+})
+export class WhyArdaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/why-arda/why-arda.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WhyArdaComponent } from './why-arda.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [WhyArdaComponent],
+  exports: [WhyArdaComponent]
+})
+export class WhyArdaModule { }
