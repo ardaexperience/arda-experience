af94aa650e591c7248233afcac6089af3ada8f0e
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-09-12 20:35:29 -0300

    chore: add the dependencies to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OwlModule } from 'ngx-owl-carousel';
+
+
 import { AppComponent } from './app.component';
+
+import { NavbarModule } from './pages/navbar/navbar.module';
+import { HeaderModule } from './pages/header/header.module';
+import { ServicesModule } from './pages/services/services.module';
+import { WhyArdaModule } from './pages/why-arda/why-arda.module';
+import { LoginModule } from './pages/login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    OwlModule,
+    NavbarModule,
+    HeaderModule,
+    ServicesModule,
+    WhyArdaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
