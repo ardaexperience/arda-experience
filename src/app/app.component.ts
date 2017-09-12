3679306c25b9bb07abd891377e70ed1a7d488963
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-09-12 20:37:19 -0300

    chore: add app component to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+
+import { WOW } from 'wowjs/dist/wow.min';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    new WOW().init();
+  }
 }
